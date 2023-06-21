5894882e0c22d51a1f9611633afd83cf64724773
ivan-teotonio <ivanteotonio256@gmail.com>
2023-06-20 23:55:02 -0300

    Crud de teste

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('companies', CompanyController::class);
